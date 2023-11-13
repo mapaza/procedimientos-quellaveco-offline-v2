8f5cc3ca7508297004ef519e6f3157f368437e4b
mapaza <mariam.apaza@tecsup.edu.pe>
2023-11-13 13:11:32 -0500

    Commit 3

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState }from 'react';
 import GlobalStyles from './styles/GlobalStyles';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import AislamientoYBloqueo from './pages/AislamientoYBloqueo';
+import TrabajosEnAltura from './pages/TrabajosEnAltura';
+import TrabajosEnCaliente from './pages/TrabajosEnCaliente';
+import TrabajosEnEspaciosConfinados from './pages/TrabajosEspaciosConfinados';
 
 const App: React.FC = () => {
   const [isInIframe, setIsInIframe] = useState(false);
@@ -26,7 +30,12 @@ const App: React.FC = () => {
     <Router>
      <Routes>
      {/*  */}
-       <Route path={rutaServidor} element={<Home />}/>
+     <Route path={rutaServidor} element={<Home />}/>
+       <Route path="/aislamiento-y-bloqueo" element={<AislamientoYBloqueo/>}/>
+       <Route path="/trabajos-en-altura" element={<TrabajosEnAltura/>}/>
+       <Route path="/trabajos-en-caliente" element={<TrabajosEnCaliente/>}/>
+       <Route path="/trabajos-en-espacios-confinados" element={<TrabajosEnEspaciosConfinados/>}/>
+           
       
        </Routes>
        <GlobalStyles />
